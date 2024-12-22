85010abb8e9adf2fb800ec1b7100cf44ebcf653d
Mario <mario.prskalo5@gmail.com>
2024-12-22 18:15:00 +0100

    implementing a linked list data structure

--- 10-More-on-Design-Patterns/src/classes/interfaces/SortableInterface.ts
+++
@@ -1,5 +0,0 @@
-export interface Sortable {
-  length: number;
-  shouldSwap(leftIndex: number, rightIndex: number): boolean;
-  swap(leftIndex: number, rightIndex: number): void;
-}
--- 10-More-on-Design-Patterns/src/index.ts
+++ 10-More-on-Design-Patterns/src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from './classes/Sorter';
 import { NumbersCollection } from './classes/NumbersCollection';
 import { CharactersCollection } from './classes/CharactersCollection';
+import { LinkedList } from './classes/LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0, 7]);
-const sorterNumbers = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0, 7]);
+// const sorterNumbers = new Sorter(numbersCollection);
 // sorterNumbers.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('ACBKDOE');
-const sorterString = new Sorter(charactersCollection);
-sorterString.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection('ACBKDOE');
+// const sorterString = new Sorter(charactersCollection);
+// sorterString.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(5);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
