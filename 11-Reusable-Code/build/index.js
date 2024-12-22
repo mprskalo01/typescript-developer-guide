6c23eb6ef12b7c49c98952cf8c32c5f014e70ff7
Mario <mario.prskalo5@gmail.com>
2024-12-22 22:10:32 +0100

    using enums to define a set of named constants for our match analysis

--- 11-Reusable-Code/build/index.js
+++ 11-Reusable-Code/build/index.js
@@ -8,4 +8,17 @@ const matches = fs_1.default
     .readFileSync('football.csv', { encoding: 'utf8' })
     .split('\n')
     .map((row) => row.split(','));
-console.log(matches);
+// enum - enumeration
+var matchResult;
+(function (matchResult) {
+    matchResult["HomeWin"] = "H";
+    matchResult["AwayWin"] = "A";
+    matchResult["Draw"] = "D";
+})(matchResult || (matchResult = {}));
+let manUnitedWins = 0;
+matches.forEach(match => {
+    if ((match[1] === 'Man United' && match[5] === matchResult.HomeWin) ||
+        (match[2] === 'Man United' && match[5] === matchResult.AwayWin))
+        manUnitedWins++;
+});
+console.log(`Man United won ${manUnitedWins} games`);
--- 11-Reusable-Code/src/index.ts
+++ 11-Reusable-Code/src/index.ts
@@ -4,5 +4,21 @@ const matches = fs
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
-  console.log(matches)
-  
+// enum - enumeration
+enum matchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+matches.forEach(match => {
+  if (
+    (match[1] === 'Man United' && match[5] === matchResult.HomeWin) ||
+    (match[2] === 'Man United' && match[5] === matchResult.AwayWin)
+  )
+    manUnitedWins++;
+});
+
+console.log(`Man United won ${manUnitedWins} games`);
