63ee9d70464860e61a8626f32998afe9059e57dc
Mario <mario.prskalo5@gmail.com>
2024-12-25 23:10:15 +0100

    implementing an eventing system with listeren support to store events in a dynamic array

--- 13-Lets-Build-a-Web-Framework/src/models/User.ts
+++ 13-Lets-Build-a-Web-Framework/src/models/User.ts
@@ -3,7 +3,10 @@ interface UserProps {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -14,5 +17,17 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) return;
+    handlers.forEach((callback: Callback): void => {
+      callback();
+    });
+  }
 }
