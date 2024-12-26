39412c6e8498f4d726e38928e8a04c70d33639ae
Mario <mario.prskalo5@gmail.com>
2024-12-26 23:01:40 +0100

    integrating passthrough methods

--- 13-Lets-Build-a-Web-Framework/src/index.ts
+++ 13-Lets-Build-a-Web-Framework/src/index.ts
@@ -1,4 +1,13 @@
 import axios from 'axios';
 import { User } from './models/User';
 
+const user = new User({ id: 1, name: 'newer name', age: 300 });
 
+user.on('change', () => {
+  console.log(user);
+});
+user.on('save', () => {
+  console.log(`User saved!`, user);
+});
+
+user.save();
--- 13-Lets-Build-a-Web-Framework/src/models/User.ts
+++ 13-Lets-Build-a-Web-Framework/src/models/User.ts
@@ -1,17 +1,54 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
+import { AxiosResponse } from 'axios';
+import { error } from 'console';
 interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
-const rootUrl = ' http://localhost:3000/users';
+const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync(rootUrl);
   public attributes: Attributes<UserProps>;
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+  get trigger() {
+    return this.events.trigger;
+  }
+  get get() {
+    return this.attributes.get;
+  }
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without id');
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
